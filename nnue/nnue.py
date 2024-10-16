893b1a602634010d1e14e278475fc09f5d6b27db
lascar pacagi <lascar.pacagi@protonmail.com>
2024-10-16 16:23:22 +0200

    nnue in pytorch

--- nnue/nnue.py
+++ nnue/nnue.py
@@ -3,45 +3,23 @@ import torch
 from torch import nn
 from tqdm import tqdm
 import re
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
 import glob
 import sys
 sys.path.append("../server")
 from yolah import Yolah, Move
 import itertools
 
-# NN_SIZE_L1 = 256
-# NN_SIZE_L2 = 32
-# NN_SIZE_L3 = 32
-# NN_SIZE_L4 = 2
-
-# # Q and -Q (+/- 1,98) are the minimum and maximum values allowed for weights and biases
-# Q = 127 / 64
-
-# # black positions + white positions + empty positions + occupied positions + turn 
-# INPUT_SIZE = 64 + 64 + 64 + 64 + 1
-
 def bitboard64_to_list(n):
     b = [int(digit) for digit in bin(n)[2:]]
     return [0]*(64 - len(b)) + b
 
+CLAMP_SCORE=10
+
 class GameDataset(Dataset):
     GAME_RE  = re.compile(r"""(\w\d:\w\d)""")
     SCORE_RE = re.compile(r"""(\d+)/(\d+)""")
-
-    @staticmethod
-    def encode(moves):
-        yolah = Yolah()
-        for m in map(lambda m: Move.from_str(m), moves):
-            yolah.play(m)
-        res = list(itertools.chain.from_iterable([
-                    bitboard64_to_list(yolah.black), 
-                    bitboard64_to_list(yolah.white), 
-                    bitboard64_to_list(yolah.empty),
-                    bitboard64_to_list(yolah.black | yolah.white | yolah.empty),
-                    [yolah.nb_plies() & 1]]))
-        return torch.tensor(res, dtype=torch.float32)
-
+    
     def __init__(self, games_dir):
         self.inputs = []
         self.outputs = []        
@@ -62,10 +40,24 @@ class GameDataset(Dataset):
                         white_score = int(white_score)
                         res = 0
                         if black_score > white_score: res = 1
-                        if white_score > black_score: res = -1
-                        self.outputs.append(torch.tensor([res, black_score - white_score], dtype=torch.float32))
+                        if white_score > black_score: res = -1                        
+                        score = min(CLAMP_SCORE, max(-CLAMP_SCORE, black_score - white_score)) / CLAMP_SCORE
+                        self.outputs.append(torch.tensor([res, score], dtype=torch.float32))
                 self.infos.append((filename, r, nb_positions))
         self.size = nb_positions
+
+    @staticmethod
+    def encode(moves):
+        yolah = Yolah()
+        for m in map(lambda m: Move.from_str(m), moves):
+            yolah.play(m)
+        res = list(itertools.chain.from_iterable([
+                    bitboard64_to_list(yolah.black), 
+                    bitboard64_to_list(yolah.white), 
+                    bitboard64_to_list(yolah.empty),
+                    bitboard64_to_list(yolah.black | yolah.white | yolah.empty),
+                    [yolah.nb_plies() & 1]]))
+        return torch.tensor(res, dtype=torch.float32)
 
     def get_infos(self):
         return self.infos
@@ -91,22 +83,85 @@ class GameDataset(Dataset):
                 hi = m
         n = self.inputs[lo - 1][0] if lo > 0 else 0
         moves = self.inputs[lo][1]
-        print(n, moves, lo, r)
+        #print(n, moves, lo, r)
         return GameDataset.encode(moves[: r + idx - n]), self.outputs[lo]
 
-# class NeuralNetwork(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.flatten = nn.Flatten()
-#         self.linear_relu_stack = nn.Sequential(
-#             nn.Linear(28*28, 512),
-#             nn.ReLU(),
-#             nn.Linear(512, 512),
-#             nn.ReLU(),
-#             nn.Linear(512, 10),
-#         )
+# # Q and -Q (+/- 1,98) are the minimum and maximum values allowed for weights and biases
+# # this opens the possibility to use a quantized version of the network post-training
+# Q = 127 / 64
 
-#     def forward(self, x):
-#         x = self.flatten(x)
-#         logits = self.linear_relu_stack(x)
-#         return logits
+# black positions + white positions + empty positions + occupied positions + turn 
+INPUT_SIZE = 64 + 64 + 64 + 64 + 1
+
+class Net(nn.Module):
+    def __init__(self, input_size=INPUT_SIZE, l1_size=1024, l2_size=512, l3_size=128):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size, l1_size)
+        self.fc2 = nn.Linear(l1_size, l2_size)
+        self.fc3 = nn.Linear(l2_size, l3_size)
+        self.fc4 = nn.Linear(l3_size, 2)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = torch.relu(x)
+        x = self.fc2(x)
+        x = torch.relu(x)
+        x = self.fc3(x)
+        x = torch.relu(x)
+        x = self.fc4(x)
+        res = torch.tanh(x[:,0])
+        score = torch.clamp(x[:,1] / CLAMP_SCORE, -1, 1)
+        return res, score
+
+NB_EPOCHS=1000
+MODEL_PATH="./nnue.pt"
+
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    dataset = GameDataset("../data")
+    print(len(dataset))
+    train_set, test_set = random_split(dataset, [0.8, 0.2])
+    print(len(train_set), len(test_set))
+    train_loader = DataLoader(train_set, batch_size=256, shuffle=True, num_workers=8)
+    test_loader = DataLoader(test_set, batch_size=256, shuffle=True, num_workers=0)
+    net = torch.load(MODEL_PATH, weights_only=False).to(device) if os.path.isfile(MODEL_PATH) else Net().to(device)
+    print(net)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    res_loss_fn = nn.CrossEntropyLoss()
+    score_loss_fn = nn.MSELoss()
+    running_loss = 0    
+    for epoch in range(NB_EPOCHS):
+        net.train()
+        for i, (X, y) in enumerate(train_loader):
+            X = X.to(device)
+            y = y.to(device)
+            optimizer.zero_grad()
+            res, score = net(X)
+            loss = 10 * res_loss_fn(res, y[:,0]) + score_loss_fn(score, y[:,1])
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            if i % 1000 == 999:
+                print('  batch {} loss: {}'.format(i + 1, running_loss / len(X) / 1000))
+                running_loss = 0
+        net.eval()
+        torch.save(net, MODEL_PATH)
+        if epoch % 10 == 9:
+            accuracy_res = 0
+            accuracy_score = 0
+            n = 0
+            res_values = torch.tensor([-1, 0, 1]).to(device)
+            for (X, y) in tqdm(test_loader):
+                n += len(X)
+                X = X.to(device)
+                y = y.to(device)            
+                res, score = net(X)
+                diff = abs(res.unsqueeze(1) - res_values)
+                nearest_indices = torch.argmin(diff, dim=1)
+                nearest_elements = res_values[nearest_indices]
+                accuracy_res += sum(nearest_elements == y[:,0])
+                accuracy_score += sum(abs(score - y[:,1])).item()
+            print('  Accuracy res: {}\n  Accuracy score: {}'.format(accuracy_res / n, 1.0 - accuracy_score / n))
+
+if __name__ == "__main__":
+    main()
