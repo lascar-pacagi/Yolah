ef36e60d1528d3d6ec49af20d5fbb151a88b55d6
elucterio <elucterio@gmail.com>
2025-03-21 11:49:54 +0100

    new representation for nnue input

--- nnue/nnue_multigpu.py
+++ nnue/nnue_multigpu.py
@@ -44,11 +44,13 @@ class GameDataset(Dataset):
                         black_score, white_score = GameDataset.SCORE_RE.findall(line)[0]                              
                         black_score = int(black_score)
                         white_score = int(white_score)
-                        if black_score == white_score: res = 1
-                        elif black_score > white_score: res = 0
-                        else: res = 2                       
-                        self.outputs.append(res)
-                        #self.outputs.append(torch.tensor(res, dtype=torch.long))
+                        res = 1
+                        if black_score > white_score: 
+                            res = 0
+                        elif white_score > black_score: 
+                            res = 2                       
+                        #self.outputs.append(res)
+                        self.outputs.append(torch.tensor(res, dtype=torch.long))
                 self.infos.append((filename, r, nb_positions))
         self.size = nb_positions
 
@@ -63,40 +65,56 @@ class GameDataset(Dataset):
     #                 [yolah.nb_plies() & 1]*64]))
     #     return torch.tensor(res, dtype=torch.float32)
 
+    # @staticmethod
+    # def encode_yolah(yolah):
+    #     if yolah.current_player() == Yolah.BLACK_PLAYER:
+    #         res = list(itertools.chain.from_iterable([
+    #                     bitboard64_to_list(yolah.black), 
+    #                     bitboard64_to_list(yolah.white), 
+    #                     bitboard64_to_list(yolah.empty)]))
+    #     else:
+    #         res = list(itertools.chain.from_iterable([
+    #                     bitboard64_to_list(yolah.white), 
+    #                     bitboard64_to_list(yolah.black), 
+    #                     bitboard64_to_list(yolah.empty)]))
+    #     return torch.tensor(res, dtype=torch.float32)
+
     @staticmethod
     def encode_yolah(yolah):
-        if yolah.current_player() == Yolah.BLACK_PLAYER:
-            res = list(itertools.chain.from_iterable([
-                        bitboard64_to_list(yolah.black), 
-                        bitboard64_to_list(yolah.white), 
-                        bitboard64_to_list(yolah.empty)]))
-        else:
-            res = list(itertools.chain.from_iterable([
-                        bitboard64_to_list(yolah.white), 
-                        bitboard64_to_list(yolah.black), 
-                        bitboard64_to_list(yolah.empty)]))
+        res = list(itertools.chain.from_iterable([
+                    bitboard64_to_list(yolah.black), 
+                    bitboard64_to_list(yolah.white), 
+                    bitboard64_to_list(yolah.empty),                    
+                    [Yolah.WHITE_PLAYER if yolah.nb_plies() & 1 else Yolah.BLACK_PLAYER]]))
         return torch.tensor(res, dtype=torch.float32)
 
+    @staticmethod
+    def encode(moves):
+        yolah = Yolah()
+        for m in map(lambda m: Move.from_str(m), moves):
+            yolah.play(m)
+        return GameDataset.encode_yolah(yolah)
+
     # @staticmethod
-    # def encode(moves):
+    # def encode(moves, res):
     #     yolah = Yolah()
     #     for m in map(lambda m: Move.from_str(m), moves):
     #         yolah.play(m)
-    #     return GameDataset.encode_yolah(yolah)
+    #     output = 1
+    #     if res == 0:
+    #         if yolah.current_player() == Yolah.BLACK_PLAYER: output = 0
+    #         else: output = 2
+    #     elif res == 2:
+    #         if yolah.current_player() == Yolah.WHITE_PLAYER: output = 0
+    #         else: output = 2
+    #     return GameDataset.encode_yolah(yolah), torch.tensor(output, dtype=torch.long)
 
     @staticmethod
     def encode(moves, res):
         yolah = Yolah()
         for m in map(lambda m: Move.from_str(m), moves):
-            yolah.play(m)
-        output = 1
-        if res == 0:
-            if yolah.current_player() == Yolah.BLACK_PLAYER: output = 0
-            else: output = 2
-        elif res == 2:
-            if yolah.current_player() == Yolah.WHITE_PLAYER: output = 0
-            else: output = 2
-        return GameDataset.encode_yolah(yolah), torch.tensor(output, dtype=torch.long)
+            yolah.play(m)        
+        return GameDataset.encode_yolah(yolah)
 
     def get_infos(self):
         return self.infos
@@ -123,13 +141,13 @@ class GameDataset(Dataset):
         n = self.inputs[lo - 1][0] if lo > 0 else 0
         moves = self.inputs[lo][1]
         #print(n, moves, lo, r)
-        return GameDataset.encode(moves[: r + idx - n], self.outputs[lo])
+        return GameDataset.encode(moves[: r + idx - n]), self.outputs[lo]
 
 # black positions + white positions + empty positions + occupied positions + free positions + turn 
 #INPUT_SIZE = 64 + 64 + 64 + 64 + 64 + 64
 
 # black positions + white positions + empty positions + turn 
-INPUT_SIZE = 64 + 64 + 64
+INPUT_SIZE = 64 + 64 + 64 + 1
 
 class Net(nn.Module):
     def __init__(self, input_size=INPUT_SIZE, l1_size=1024, l2_size=32, l3_size=64):
